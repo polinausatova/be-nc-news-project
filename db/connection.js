d3d0dab0927e937babcd935a847d8db24ec71c49
Polina Usatova <polina@Polinas-MacBook-Pro.local>
2023-02-24 14:20:09 +0000

    T14 host api changes

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,24 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
+
+module.exports = new Pool(config);
+
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 console.log(`the node enviroment is ${ENV}`);
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
 module.exports = new Pool();
